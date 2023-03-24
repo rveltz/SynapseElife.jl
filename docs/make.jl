2decfe5df00c080f3a33e1b2fa96fbd110dd7a63
romain veltz <romain.veltz@inria.fr>
2023-03-24 16:48:18 +0100

    simplifed docs config

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,8 @@
 using Documenter, Synapse, Setfield
 
-const makepdf = false
-
-if makepdf
-	using DocumenterLaTeX
-end
-
 makedocs(doctest = false,
 	sitename = "Model of excitatory synapse in Julia",
-	format = makepdf ? DocumenterLaTeX.LaTeX(platform = "none") : Documenter.HTML(collapselevel = 1),
+	format = false,
 	pages = Any[
 		"Home" => "index.md",
 		# "Simple example" => "tutorials.md",
