970f011796b83e1271b882e6bd8a190da31574fe
romain veltz <romain.veltz@inria.fr>
2023-03-24 15:24:12 +0100

    simplified version of the docs

--- docs/make.jl
+++ docs/make.jl
@@ -11,8 +11,8 @@ makedocs(doctest = false,
 	format = makepdf ? DocumenterLaTeX.LaTeX(platform = "none") : Documenter.HTML(collapselevel = 1),
 	pages = Any[
 		"Home" => "index.md",
-		"Simple example" => "tutorials.md",
-		"Figure 1" => "figure1.md",
+		# "Simple example" => "tutorials.md",
+		# "Figure 1" => "figure1.md",
 		"Library" => "library.md"
 	]
 	)
