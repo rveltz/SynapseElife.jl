cbf44a795068888b5ba5f35b9b97d6245934b432
mediavince <developer@mediavince.com>
2026-03-13 19:49:47 +0100

    cleanup

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,23 @@
-using Documenter, SynapseElife
 using Pkg
-# pkg"dev Plots"
+pkg"dev Documenter"
 
-ENV["JULIA_DEBUG"]=Documenter
+using Documenter, SynapseElife
 
-makedocs(doctest = false,
-	sitename = "Model of excitatory synapse in Julia",
-	format = Documenter.HTML(collapselevel = 1),
-	pages = Any[
-		"Home" => "index.md",
-		"Simple example" => "tutorials.md",
-		"Figure 1" => "figure1.md",
-		"Library" => "library.md"
-	]
-	)
-	
+ENV["JULIA_DEBUG"] = Documenter
+
+makedocs(
+    doctest = false,
+    sitename = "Model of excitatory synapse in Julia",
+    format = Documenter.HTML(collapselevel = 1),
+    pages = Any[
+        "Home" => "index.md",
+        "Simple example" => "tutorials.md",
+        "Figure 1" => "figure1.md",
+        "Library" => "library.md",
+    ]
+)
+
 deploydocs(
-	repo = "github.com/rveltz/SynapseElife.jl.git",
-	devbranch = "main"
+    repo = "github.com/rveltz/SynapseElife.git",
+    devbranch = "main"
 )
